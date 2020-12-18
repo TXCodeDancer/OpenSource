697e0d2909ec549ecd48754c1f5b7e2f6b0f71a5
Rick Brunson <brunson1@slb.com>
2020-12-18 17:45:09 -0600

    [GraphViz] Implement Library

--- Examples/QuikGraph/Visualize/Main/Program.cs
+++ Examples/QuikGraph/Visualize/Main/Program.cs
@@ -1,92 +1,38 @@
 ﻿using QuikGraph;
-using QuikGraph.Algorithms.Search;
-using QuikGraph.Graphviz;
 using QuikGraph.Graphviz.Dot;
-using System.Diagnostics.Contracts;
-using System.Globalization;
 using System.IO;
+using Visualize;
 
 namespace Main
 {
     internal class Program
     {
-        public static void ShowPicture(string filename)
-        {
-            // How to open file with default viewer?
-            //             using (Form f = new Form())
-            //             {
-            //                 // f.FormBorderStyle = FormBorderStyle.None;
-            //
-            //                 PictureBox picture = new PictureBox()
-            //                 {
-            //                     ImageLocation = filename,
-            //                     SizeMode = PictureBoxSizeMode.Normal,
-            //                     Dock = DockStyle.Fill,
-            //                     Size = new Size(100, 300),
-            //                 };
-            //                 f.Controls.Add(picture);
-            //                 f.Size = picture.Size;
-            //
-            //                 f.ShowDialog();
-            //                 f.Refresh();
-            //                 f.Show();
-            //             }
-        }
-
-        private sealed class FileDotEngine : IDotEngine
-        {
-            public string Run(GraphvizImageType imageType, string dot, string outputFilePath, string layout)
-            {
-                File.WriteAllText(outputFilePath + ".dot", dot);
-                string type = imageType.ToString().ToLower();
-
-                if (true)
-                {
-                    // assumes graphviz is on the path:
-                    string args = string.Format(CultureInfo.InvariantCulture, $"{outputFilePath}.dot -T{type} -O");
-                    System.Diagnostics.Process process = System.Diagnostics.Process.Start($"{layout}.exe", args);
-                    if (true)
-                    {
-                        process.WaitForExit();
-                        // ShowPicture(filename + ".pdf");
-                    }
-                }
-                return outputFilePath;
-            }
-
-            public string Run(GraphvizImageType imageType, string dot, string outputFilePath)
-            {
-                var fileInfoList = outputFilePath.Split(' ');
-                string filename = fileInfoList[0];
-                string layoutEngine = "dot";
-                if (fileInfoList.Length > 1)
-                    layoutEngine = fileInfoList[1];
-
-                return Run(imageType, dot, filename, layoutEngine);
-            }
-        }
-
         private static void Main(string[] args)
         {
+            string dir = @"D:\Samples\QuikGraph\QuikSample\Graphs";
             AdjacencyGraph<int, Edge<int>> displayGraph1 = GetGraph1();
 
             string filename = "test1";
-            ExportDot(displayGraph1, filename);
-            CreateImageFile(displayGraph1, GraphvizImageType.Svg, filename);
+            string filepath = Path.Combine(dir, filename);
+            Visualizer.ExportDot(displayGraph1, filepath);
+            Visualizer.CreateImageFile(displayGraph1, GraphvizImageType.Svg, filepath, ImageLayout.dot);
 
             var displayGraph2 = GetGraph2();
 
             filename = "test2";
-            ExportDot(displayGraph2, filename);
-            CreateImageFile(displayGraph2, GraphvizImageType.Svg, filename);
+            filepath = Path.Combine(dir, filename);
+            Visualizer.ExportDot(displayGraph2, filepath);
+            Visualizer.VertexShape = GraphvizVertexShape.Box;
+            Visualizer.VertexStyle = GraphvizVertexStyle.Rounded;
+            Visualizer.CreateImageFile(displayGraph2, GraphvizImageType.Svg, filepath, ImageLayout.circo);
 
-            var dfs = new DepthFirstSearchAlgorithm<int, Edge<int>>(displayGraph1);
-            //do the search
-            dfs.Compute();
-
-            var dfsGraph = dfs.VisitedGraph;
-            filename = "test3";
-            CreateImageFile((AdjacencyGraph<int, Edge<int>>)dfsGraph, GraphvizImageType.Svg, filename);
+            //             var dfs = new DepthFirstSearchAlgorithm<int, Edge<int>>(displayGraph1);
+            //             //do the search
+            //             dfs.Compute();
+            //
+            //             var dfsGraph = dfs.VisitedGraph;
+            //             filename = "test3";
+            //             CreateImageFile((AdjacencyGraph<int, Edge<int>>)dfsGraph, GraphvizImageType.Svg, filename);
         }
 
         private static AdjacencyGraph<int, Edge<int>> GetGraph1()
@@ -145,90 +91,6 @@ namespace Main
             }
 
             return displayGraph;
-        }
-
-        private static void ExportDot(AdjacencyGraph<int, Edge<int>> graph, string filename, string dir = @"D:\Samples\QuikGraph\QuikSample\Graphs")
-        {
-            string fullFileName = Path.Combine(dir, filename);
-            var viz = new GraphvizAlgorithm<int, Edge<int>>(graph);
-
-            var dotfile = fullFileName;
-            viz.Generate(new FileDotEngine(), dotfile);
-        }
-
-        private static void ExportDot(AdjacencyGraph<string, TaggedEdge<string, string>> graph, string filename, string dir = @"D:\Samples\QuikGraph\QuikSample\Graphs")
-        {
-            string fullFileName = Path.Combine(dir, filename);
-            var viz = new GraphvizAlgorithm<string, TaggedEdge<string, string>>(graph);
-
-            var dotfile = fullFileName;
-            viz.Generate(new FileDotEngine(), dotfile);
-        }
-
-        private static void FormatVertexHandler(object sender, FormatVertexEventArgs<int> e)
-        {
-            Contract.Requires(e != null);
-
-            e.VertexFormat.Label = e.Vertex.ToString();
-            e.VertexFormat.Shape = GraphvizVertexShape.Box;
-            e.VertexFormat.Style = GraphvizVertexStyle.Rounded;
-        }
-
-        private static void FormatVertexHandler(object sender, FormatVertexEventArgs<string> e)
-        {
-            Contract.Requires(e != null);
-
-            e.VertexFormat.Label = e.Vertex;
-            e.VertexFormat.Shape = GraphvizVertexShape.Box;
-            e.VertexFormat.Style = GraphvizVertexStyle.Rounded;
-        }
-
-        private static string CreateImageFile(AdjacencyGraph<int, Edge<int>> graph, GraphvizImageType imageType, string imageFileName, string dir = @"D:\Samples\QuikGraph\QuikSample\Graphs")
-        {
-            var graphviz = new GraphvizAlgorithm<int, Edge<int>>(graph)
-            {
-                ImageType = imageType
-            };
-
-            graphviz.FormatVertex += FormatVertexHandler;
-
-            string outputfile = Path.Combine(dir, imageFileName + "_svg");
-            string layout = "circo";
-
-            graphviz.Generate(new FileDotEngine(), outputfile + " " + layout);
-            return outputfile;
-        }
-
-        private static string CreateImageFile(AdjacencyGraph<string, TaggedEdge<string, string>> graph, GraphvizImageType imageType, string imageFileName, string dir = @"D:\Samples\QuikGraph\QuikSample\Graphs")
-        {
-            var graphviz = new GraphvizAlgorithm<string, TaggedEdge<string, string>>(graph)
-            {
-                ImageType = imageType
-            };
-
-            graphviz.FormatVertex += FormatVertexHandler;
-            graphviz.FormatEdge += MyEdgeFormatter;
-
-            string outputfile = Path.Combine(dir, imageFileName + "_svg");
-            string layout = "circo";
-
-            graphviz.Generate(new FileDotEngine(), outputfile + " " + layout);
-            return outputfile;
-        }
-
-        private static void MyEdgeFormatter(object sender, FormatEdgeEventArgs<string, TaggedEdge<string, string>> e)
-        {
-            GraphvizEdgeLabel label = new GraphvizEdgeLabel
-            {
-                Value = e.Edge.Tag,
-            };
-            e.EdgeFormat.Label = label;
-        }
-
-        private static void VizFormatVertex(object sender, FormatVertexEventArgs<string> e)
-        {
-            Contract.Requires(e != null);
-            e.VertexFormat.Label = e.Vertex.ToString(CultureInfo.InvariantCulture);
         }
     }
 }
--- Examples/QuikGraph/Visualize/Visualize/Visualize.cs
+++ Examples/QuikGraph/Visualize/Visualize/Visualize.cs
@@ -1,6 +1,8 @@
-﻿using QuikGraph.Graphviz;
+﻿using QuikGraph;
+using QuikGraph.Graphviz;
 using QuikGraph.Graphviz.Dot;
 using System;
+using System.Diagnostics.Contracts;
 using System.Globalization;
 using System.IO;
 
@@ -8,41 +10,129 @@ namespace Visualize
 {
     public enum ImageLayout
     {
-        None = 0,
+        none = 0,
+        dot,
+        neato,
+        twopi,
+        circo,
+        fdp,
+        sfdp,
+        patchwork,
+        osage
     }
 
     public static class Visualizer
     {
+        private static GraphvizVertexShape _VertexShape = GraphvizVertexShape.Ellipse;
+        private static GraphvizVertexStyle _VertexStyle = GraphvizVertexStyle.Rounded;
+
+        public static GraphvizVertexShape VertexShape
+        {
+            get { return _VertexShape; }
+            set { _VertexShape = value; }
+        }
+
+        public static GraphvizVertexStyle VertexStyle
+        {
+            get { return _VertexStyle; }
+            set { _VertexStyle = value; }
+        }
+
         private sealed class FileDotEngine : IDotEngine
         {
-            public string Run(GraphvizImageType imageType, string dot, string outputFilePath, string layout)
+            public string Run(GraphvizImageType imageType, string dot, string outputFilePath, ImageLayout layout)
             {
                 File.WriteAllText(outputFilePath + ".dot", dot);
                 string type = imageType.ToString().ToLower();
 
-                if (true)
+                if (layout != ImageLayout.none)
                 {
                     // assumes graphviz is on the path:
-                    string args = string.Format(CultureInfo.InvariantCulture, $"{outputFilePath}.dot -T{type} -O");
+                    string args = string.Format(CultureInfo.InvariantCulture, $"{outputFilePath}.dot -T{type} -o {outputFilePath}_{layout}.{type}");
                     System.Diagnostics.Process process = System.Diagnostics.Process.Start($"{layout}.exe", args);
                     if (true)
                     {
                         process.WaitForExit();
                     }
                 }
+
                 return outputFilePath;
             }
 
-            public string Run(GraphvizImageType imageType, string dot, string outputFilePath)
+            public string Run(GraphvizImageType imageType, string dotContents, string fileInfo)
             {
-                var fileInfoList = outputFilePath.Split(' ');
-                string filename = fileInfoList[0];
-                string layoutEngine = "dot";
-                if (fileInfoList.Length > 1)
-                    layoutEngine = fileInfoList[1];
+                var filepath = fileInfo.Split(' ');
+                string filename = filepath[0];
+                ImageLayout layout = ImageLayout.none;
+                if (filepath.Length > 1)
+                {
+                    Enum.TryParse(filepath[1], true, out layout);
+                }
 
-                return Run(imageType, dot, filename, layoutEngine);
+                return Run(imageType, dotContents, filename, layout);
             }
+        }
+
+        public static void ExportDot(AdjacencyGraph<int, Edge<int>> graph, string filepath)
+        {
+            var viz = new GraphvizAlgorithm<int, Edge<int>>(graph);
+            viz.Generate(new FileDotEngine(), filepath);
+        }
+
+        public static void ExportDot(AdjacencyGraph<string, TaggedEdge<string, string>> graph, string filepath)
+        {
+            var viz = new GraphvizAlgorithm<string, TaggedEdge<string, string>>(graph);
+            viz.Generate(new FileDotEngine(), filepath);
+        }
+
+        public static string CreateImageFile(AdjacencyGraph<int, Edge<int>> graph, GraphvizImageType imageType, string imageFilepath, ImageLayout layout)
+        {
+            var graphviz = new GraphvizAlgorithm<int, Edge<int>>(graph) { ImageType = imageType };
+
+            graphviz.FormatVertex += FormatVertexHandler;
+
+            string fileInfo = $"{imageFilepath} {layout}";
+            graphviz.Generate(new FileDotEngine(), fileInfo);
+            return imageFilepath;
+        }
+
+        public static string CreateImageFile(AdjacencyGraph<string, TaggedEdge<string, string>> graph, GraphvizImageType imageType, string imageFilepath, ImageLayout layout)
+        {
+            var graphviz = new GraphvizAlgorithm<string, TaggedEdge<string, string>>(graph) { ImageType = imageType };
+
+            graphviz.FormatVertex += FormatVertexHandler;
+            graphviz.FormatEdge += MyEdgeFormatter;
+
+            string fileInfo = $"{imageFilepath} {layout}";
+            graphviz.Generate(new FileDotEngine(), fileInfo);
+            return imageFilepath;
+        }
+
+        private static void FormatVertexHandler(object sender, FormatVertexEventArgs<string> e)
+        {
+            Contract.Requires(e != null);
+
+            e.VertexFormat.Label = e.Vertex;
+            e.VertexFormat.Shape = _VertexShape;
+            e.VertexFormat.Style = _VertexStyle;
+        }
+
+        private static void FormatVertexHandler(object sender, FormatVertexEventArgs<int> e)
+        {
+            Contract.Requires(e != null);
+
+            e.VertexFormat.Label = e.Vertex.ToString();
+            e.VertexFormat.Shape = _VertexShape;
+            e.VertexFormat.Style = _VertexStyle;
+        }
+
+        private static void MyEdgeFormatter(object sender, FormatEdgeEventArgs<string, TaggedEdge<string, string>> e)
+        {
+            GraphvizEdgeLabel label = new GraphvizEdgeLabel
+            {
+                Value = e.Edge.Tag,
+            };
+            e.EdgeFormat.Label = label;
         }
     }
 }
