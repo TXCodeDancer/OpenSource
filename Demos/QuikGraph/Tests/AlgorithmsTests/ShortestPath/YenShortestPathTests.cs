ae1c7cb9a0a844339127d630786c0d7c6c2cedec
Rick Brunson <brunson1@slb.com>
2021-01-07 16:54:01 -0600

    Update Yen Shortest Path library

--- Demos/QuikGraph/Tests/AlgorithmsTests/ShortestPath/YenShortestPathTests.cs
+++ Demos/QuikGraph/Tests/AlgorithmsTests/ShortestPath/YenShortestPathTests.cs
@@ -45,6 +45,41 @@ namespace Tests.AlgorithmsTests.ShortestPath
             }
         }
 
+        [Theory]
+        [MemberData(nameof(GetInputFiles))]
+        public void Test1(string inputFile)
+        {
+            string directory = Path.GetDirectoryName(inputFile);
+            string file = Path.GetFileNameWithoutExtension(inputFile);
+            var outputFile = @$"{directory}\ShortestPath\Yen\AllPaths\{file}";
+            var expectedfile = @$"{outputFile}.a";
+            var resultsfile = $"{outputFile}.r";
+
+            List<string> inputs = File.ReadAllLines(inputFile).ToList();
+            var nodes = inputs[0].Split(' ').ToList(); // First line is a space delimited list of node names: "1 2 3" or "a b c"
+
+            // Remove line of node name
+            inputs.RemoveAt(0);
+            List<List<string>> edges = new List<List<string>>();
+            foreach (var e in inputs)
+            {
+                edges.Add(e.Split(' ').ToList()); // Remaining lines are space delimited list of edges (nodeA nodeB tag(optional)):  "1 2" or "a b 5"
+            }
+
+            var root = nodes[0];
+            var target = nodes[nodes.Count - 1];
+            List<string> actual = ShortestPathHelper.YenAllPathsHelper(nodes, edges, root, target);
+            File.WriteAllLines(resultsfile, actual);
+
+            // Verify results
+            List<string> expected = File.ReadAllLines(expectedfile).ToList();
+            Assert.Equal(expected.Count, actual.Count);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
+            }
+        }
+
         public static IEnumerable<object[]> GetInputFiles =>
             new List<object[]>
             {
