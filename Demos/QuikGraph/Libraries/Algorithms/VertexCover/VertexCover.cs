586d607ebeeea2273051962428d830387c0522fe
Rick Brunson <brunson1@slb.com>
2021-01-12 16:42:53 -0600

    Adding Comment for VertexCover seed parameter

--- Demos/QuikGraph/Libraries/Algorithms/VertexCover/VertexCover.cs
+++ Demos/QuikGraph/Libraries/Algorithms/VertexCover/VertexCover.cs
@@ -7,14 +7,14 @@ namespace Algorithms.VertexCover
 {
     public static class VertexCover
     {
-        public static VertexList<int> Get(UndirectedGraph<int, Edge<int>> g, int seed = 123456) // Seeding with 456789, etc. may provide different CoverSet count
+        public static VertexList<int> Get(UndirectedGraph<int, Edge<int>> g, int seed = 123456) // Seeding with 123456 for testing. Seeding with 456789, etc. may provide different CoverSet count
         {
             var algorithm = new MinimumVertexCoverApproximationAlgorithm<int, Edge<int>>(g, new Random(seed));
             algorithm.Compute();
             return algorithm.CoverSet;
         }
 
-        public static VertexList<string> Get(UndirectedGraph<string, TaggedEdge<string, string>> g, int seed = 123456) // Seeding with 456789, etc. may provide different CoverSet count
+        public static VertexList<string> Get(UndirectedGraph<string, TaggedEdge<string, string>> g, int seed = 123456) // Seeding with 123456 for testing. Seeding with 456789, etc. may provide different CoverSet count
         {
             var algorithm = new MinimumVertexCoverApproximationAlgorithm<string, TaggedEdge<string, string>>(g, new Random(seed));
             algorithm.Compute();
