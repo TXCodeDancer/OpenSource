9fe688e42f81943a1d6b89c305347526406653b9
RB <rbrunson01@gmail.com>
2021-04-24 11:57:00 -0500

    Add tests for Square Class

--- Demos/UnitTestDemo/MathTests/SquareTests.cpp
+++ Demos/UnitTestDemo/MathTests/SquareTests.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "Square.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,6 +12,25 @@ namespace MathTests
 
         TEST_METHOD(TestGetSqare)
         {
+            int seed = 3;
+            int expected = 9;
+            auto sq = Square(seed);
+            auto seedSquared = sq.GetSquare();
+            Assert::AreEqual(expected, seedSquared);
+
+            int side = 5;
+            auto area = sq.GetArea(side);
+        }
+
+        TEST_METHOD(TestGetArea)
+        {
+            int seed = 3;
+            int expected = 25;
+            auto sq = Square(seed);
+
+            int side = 5;
+            auto area = sq.GetArea(side);
+            Assert::AreEqual(expected, area);
         }
     };
 }
