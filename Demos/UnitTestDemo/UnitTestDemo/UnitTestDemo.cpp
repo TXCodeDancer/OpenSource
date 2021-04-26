e3d460b4584c490ad8e5076aca069c1702e16865
Rick Brunson <brunson1@slb.com>
2021-04-26 13:17:15 -0500

    Fix Build

--- Demos/UnitTestDemo/UnitTestDemo/UnitTestDemo.cpp
+++ Demos/UnitTestDemo/UnitTestDemo/UnitTestDemo.cpp
@@ -2,16 +2,16 @@
 //
 
 #include <iostream>
-#include "Square.h"
+//#include "Square.h"
 
 int main()
 {
-    int seed = 3;
-    auto sq = Square(seed);
-    auto seedSquared = sq.GetSquare();
-    std::cout << "The square of " << seed << " is " << seedSquared << "\n";
+    //int seed = 3;
+    //auto sq = Square(seed);
+    //auto seedSquared = sq.GetSquare();
+    //std::cout << "The square of " << seed << " is " << seedSquared << "\n";
 
-    int side = 5;
-    auto area = sq.GetArea(side);
-    std::cout << "The area of a square with sides of " << side << " is " << area << "\n";
+    //int side = 5;
+    //auto area = sq.GetArea(side);
+    //std::cout << "The area of a square with sides of " << side << " is " << area << "\n";
 }
