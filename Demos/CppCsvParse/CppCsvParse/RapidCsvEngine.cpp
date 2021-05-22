30bc87760f7f7d45ac43b3e2cabd68bf43f1ac18
RB <rbrunson01@gmail.com>
2021-05-22 15:02:30 -0500

    Remove CSVWriter library

--- Demos/CppCsvParse/CppCsvParse/RapidCsvEngine.cpp
+++ Demos/CppCsvParse/CppCsvParse/RapidCsvEngine.cpp
@@ -1,11 +1,8 @@
 #include "codecvt"
 #define HAS_CODECVT
 #include "RapidCsvEngine.h"
-#include "CSVWriter.h"
 #include <boost/format.hpp>
 #include <iomanip>
-
-//using namespace boost;
 
 void RapidCsvEngine::Run(string csvInput, string csvOutput)
 {
@@ -15,19 +12,14 @@ void RapidCsvEngine::Run(string csvInput, string csvOutput)
     std::vector<double> radians = doc.GetColumn<double>("Radians");
 
     auto count = angle.size();
-    CSVWriter csvWriter;
-    csvWriter.newRow() << "Angle" << "Radians";
     std::stringstream  ss;
+    ss << "Angle" << "Radians" << '\n';
     for (size_t i = 0; i < count; i++)
     {
         string csv = str(boost::format("%1$.1f,%2$.9f\n") % angle[i] % radians[i]);
-        //oss << setprecision(1) << angle[i] << ',' << setprecision(10) << radians[i];
-        //string csv2 = oss.str();
         ss << angle[i] << ',' << setprecision(10) << radians[i] << '\n';
         string csv3 = ss.str();
-        csvWriter.newRow() << angle[i] << radians[i];
     }
-    //    csvWriter.writeToFile(csvOutput);
 
     std::ofstream outFile(csvOutput);
     outFile << ss.rdbuf();
