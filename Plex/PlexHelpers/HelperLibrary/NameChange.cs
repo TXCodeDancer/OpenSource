e44667465978801e1791af851c75cc842eeccacc
RB <rbrunson01@gmail.com>
2022-01-21 13:10:49 -0600

    WIP: Tests pass

--- Plex/PlexHelpers/HelperLibrary/NameChange.cs
+++ Plex/PlexHelpers/HelperLibrary/NameChange.cs
@@ -2,10 +2,23 @@
 {
     public class NameChange
     {
-        public static string ConvertSpaceToDots(string name)
+        public static string ConvertSpace(string name, char replacement = '.')
         {
-            string newName = "";
-            return newName;
+            name = name.Replace(" ", replacement.ToString());
+            name = name.Replace("-", "");
+            name = name.Replace("_", "");
+
+            string result = "";
+            for (int i = 0; i < name.Length - 1; i++)
+            {
+                if (name[i] != replacement || name[i] != name[i + 1])
+                {
+                    result += name[i];
+                }
+            }
+            result += name[name.Length - 1];
+
+            return result;
         }
     }
 }
--- Plex/PlexHelpers/HelperLibraryTest/NameChangeTests.cs
+++ Plex/PlexHelpers/HelperLibraryTest/NameChangeTests.cs
@@ -9,13 +9,14 @@ namespace HelperLibraryTest
         public static IEnumerable<object[]> GetNames()
         {
             yield return new object[] { "Star Trek - s02e02 Who Mourns for Adonais_.mp4", "Star.Trek.s02e02.Who.Mourns.for.Adonais.mp4" };
+            yield return new object[] { "Star Trek - s02e26 Assignment_ Earth.mp4", "Star.Trek.s02e26.Assignment.Earth.mp4" };
         }
 
         [Theory]
         [MemberData(nameof(GetNames))]
         public void ConvertSpacesToDots(string input, string expected)
         {
-            var actual = NameChange.ConvertSpaceToDots(input);
+            var actual = NameChange.ConvertSpace(input);
             Assert.Equal(expected, actual);
         }
     }
