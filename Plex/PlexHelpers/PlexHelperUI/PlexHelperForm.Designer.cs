5ffe9f3983394dba394c4f5c22c57b7be83081ca
RB <rbrunson01@gmail.com>
2022-01-21 14:32:56 -0600

    WIP: File selection UI

--- Plex/PlexHelpers/PlexHelperUI/PlexHelperForm.Designer.cs
+++ Plex/PlexHelpers/PlexHelperUI/PlexHelperForm.Designer.cs
@@ -28,19 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.directoryTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "\"mp4\"";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "\"video files (*.mp4)|*.txt|All files (*.*)|*.*\"";
+            this.openFileDialog1.InitialDirectory = "@\"V:\\\"";
+            this.openFileDialog1.RestoreDirectory = true;
+            this.openFileDialog1.Title = "\"Browse Video Files\"";
+            // 
+            // browseButton
+            // 
+            this.browseButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.browseButton.Location = new System.Drawing.Point(633, 29);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(141, 51);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // directoryTextBox
+            // 
+            this.directoryTextBox.Location = new System.Drawing.Point(12, 44);
+            this.directoryTextBox.Name = "directoryTextBox";
+            this.directoryTextBox.Size = new System.Drawing.Size(615, 23);
+            this.directoryTextBox.TabIndex = 1;
             // 
             // PlexHelperForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.directoryTextBox);
+            this.Controls.Add(this.browseButton);
             this.Name = "PlexHelperForm";
             this.Text = "Plex Helper";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private Button browseButton;
+        private TextBox directoryTextBox;
     }
 }
--- Plex/PlexHelpers/PlexHelperUI/PlexHelperForm.cs
+++ Plex/PlexHelpers/PlexHelperUI/PlexHelperForm.cs
@@ -1,20 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace PlexHelperUI
+﻿namespace PlexHelperUI
 {
     public partial class PlexHelperForm : Form
     {
         public PlexHelperForm()
         {
             InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"V:\",
+                Title = "Browse Video Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "mp4",
+                Filter = "video files (*.mp4)|*.mp4",
+                FilterIndex = 1,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                directoryTextBox.Text = openFileDialog1.FileName;
+            }
         }
     }
 }
