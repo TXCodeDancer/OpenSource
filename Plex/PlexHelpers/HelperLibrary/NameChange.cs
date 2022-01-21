cf11b64cb46d002a9da93671152422032ba44054
RB <rbrunson01@gmail.com>
2022-01-21 13:20:16 -0600

    WIP: Tests cleanup

--- Plex/PlexHelpers/HelperLibrary/NameChange.cs
+++ Plex/PlexHelpers/HelperLibrary/NameChange.cs
@@ -4,19 +4,19 @@
     {
         public static string ConvertSpace(string name, char replacement = '.')
         {
+            name = name.Trim();
             name = name.Replace(" ", replacement.ToString());
             name = name.Replace("-", "");
             name = name.Replace("_", "");
 
-            string result = "";
-            for (int i = 0; i < name.Length - 1; i++)
+            string result = name[0].ToString();
+            for (int i = 1; i < name.Length; i++)
             {
-                if (name[i] != replacement || name[i] != name[i + 1])
+                if (name[i] != replacement || name[i] != name[i - 1])
                 {
                     result += name[i];
                 }
             }
-            result += name[name.Length - 1];
 
             return result;
         }
