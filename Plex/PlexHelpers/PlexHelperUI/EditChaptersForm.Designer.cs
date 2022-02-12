14b93234cdf19cfef358f8d344001ed7fa1ea8ce
RB <rbrunson01@gmail.com>
2022-02-11 20:17:30 -0600

    WIP: Edit Chapters Select File

--- Plex/PlexHelpers/PlexHelperUI/EditChaptersForm.Designer.cs
+++ Plex/PlexHelpers/PlexHelperUI/EditChaptersForm.Designer.cs
@@ -28,20 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.videoFileTextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.saveButton);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.selectButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(640, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(160, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.saveButton.Location = new System.Drawing.Point(3, 78);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(141, 51);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(3, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Source";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // selectButton
+            // 
+            this.selectButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.selectButton.Location = new System.Drawing.Point(3, 12);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(141, 51);
+            this.selectButton.TabIndex = 1;
+            this.selectButton.Text = "Select";
+            this.selectButton.UseVisualStyleBackColor = true;
+            this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
+            // 
+            // videoFileTextBox
+            // 
+            this.videoFileTextBox.Location = new System.Drawing.Point(12, 30);
+            this.videoFileTextBox.Name = "videoFileTextBox";
+            this.videoFileTextBox.Size = new System.Drawing.Size(622, 23);
+            this.videoFileTextBox.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "mp4";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "video files (*.mp4)|*.mp4|All files (*.*)|*.*";
+            this.openFileDialog1.FilterIndex = 2;
+            this.openFileDialog1.InitialDirectory = "V:\\";
+            this.openFileDialog1.Title = "Select Video File";
             // 
             // EditChaptersForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.videoFileTextBox);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "EditChaptersForm";
             this.Text = "EditChaptersForm";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Panel panel1;
+        private Button saveButton;
+        private Button button1;
+        private Button selectButton;
+        private TextBox videoFileTextBox;
+        private OpenFileDialog openFileDialog1;
     }
 }
--- Plex/PlexHelpers/PlexHelperUI/EditChaptersForm.cs
+++ Plex/PlexHelpers/PlexHelperUI/EditChaptersForm.cs
@@ -1,20 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace PlexHelperUI
+﻿namespace PlexHelperUI
 {
     public partial class EditChaptersForm : Form
     {
         public EditChaptersForm()
         {
             InitializeComponent();
+        }
+
+        private void selectButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"V:\",
+                Title = "Browse Video Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "mp4",
+                Filter = "mp4 video files (*.mp4)|*.mp4|All files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                videoFileTextBox.Text = openFileDialog1.FileName;
+            }
         }
     }
 }
