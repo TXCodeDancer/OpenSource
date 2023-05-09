c3d6904c1b230c67e83593d04a2f08a4d53d325b
Rick Brunson <brunson1@slb.com>
2023-05-09 12:30:30 -0500

    WIP: Add starter code

---
+++ ToolBox/TimeStamps/TimeStamps/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TimeStamps;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Enter DateTime arguments: ");
+        var input = Console.ReadLine();
+        if (input == null) { throw new ArgumentNullException(input); }
+
+        var arguments = input.Split(' ');
+
+        Console.WriteLine($"Number of argument = {arguments.Length}");
+
+        foreach (var argument in arguments)
+        {
+            Console.WriteLine($"{argument}");
+        }
+    }
+}
